a89b4ced68e01c14ac0020a77f51458df468da07
Paradis4432 <49301263+Paradis4432@users.noreply.github.com>
2021-05-21 15:49:43 -0300

    Delete ToxicChestJob directory

--- ToxicChestJob/src/main/java/me/paradis/main/events/CheckSlot.java
+++
@@ -1,9 +0,0 @@
-package me.paradis.main.events;
-
-public class CheckSlot {
-
-    public boolean isOutSide(Integer slot, Integer size){
-        //if slot is any of the outside ones then return true
-        return slot <= 8 || slot % 9 == 0 || (slot + 1) % 9 == 0 || slot >= (size - 9);
-    }
-}
